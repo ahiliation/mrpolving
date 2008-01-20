b9bf23e390e89a5bd9b0e1b76f3f92ca1e85755f
ahiliation <ahiliation@377b6e22-f743-0410-ae81-ab1ae32e3be8>
2008-01-20 08:40:21 +0000

    /* Jeffrin Jose <ahiliation@yahoo.co.in>  Licensed GPL v3 Copyright 2008 GPL --> http://www.gnu.org/copyleft/gpl.html */ #include <stdio.h> main() { int a,b,c; printf(" \n Enter the numbers  \n"); scanf("%d \n %d", &a , &b); c= a + b; printf (" \n The result is .... %d \n ",c); return (0); }

---
+++ trunk/addition.c
@@ -0,0 +1,12 @@
+/* Jeffrin Jose <ahiliation@yahoo.co.in>  Licensed GPL v3 Copyright 2008
+GPL --> http://www.gnu.org/copyleft/gpl.html */
+#include <stdio.h>
+main()
+{
+int a,b,c;
+printf(" \n Enter the numbers  \n");
+scanf("%d \n %d", &a , &b);
+c= a + b;
+printf (" \n The result is .... %d \n ",c);
+return (0);
+}
